1eacfef276e9f3c85cf583b0a92ade2072c36c01
Marcos Felix <marcosfelixcosta@gmail.com>
2022-07-23 16:59:29 -0300

    Implementação Classe categoria

--- src/main/java/com/marcos/petshop/resurces/CategoriaResurce.java
+++ src/main/java/com/marcos/petshop/resurces/CategoriaResurce.java
@@ -1,8 +1,13 @@
 package com.marcos.petshop.resurces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcos.petshop.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -11,8 +16,19 @@ public class CategoriaResurce {
 	
 
 @RequestMapping(method = RequestMethod.GET)
-	public String mostrar() {
-		return "Teste REST Categorias";
+	public List<Categoria> mostrar() {
+	 
+	Categoria cat1 = new Categoria(1, "Alimento");
+	Categoria cat2 = new Categoria(2, "Cosmético");
+	Categoria cat3 = new Categoria(3, "Remédio");
+	
+	List<Categoria> lista = new ArrayList<>();
+	
+	lista.add(cat1);
+	lista.add(cat2);
+	lista.add(cat3);
+	
+	return lista;
 	}
 
 }
