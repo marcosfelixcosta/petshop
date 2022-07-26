ef94fcec3482d484e545c69b4ffbd42c7420c5bc
Marcos Felix <marcosfelixcosta@gmail.com>
2022-07-26 13:53:12 -0300

    Carição do repository produtos

---
+++ src/main/java/com/marcos/petshop/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.marcos.petshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.marcos.petshop.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
--- src/main/java/com/marcos/petshop/utils/PopulaDados.java
+++ src/main/java/com/marcos/petshop/utils/PopulaDados.java
@@ -1,5 +1,6 @@
 package com.marcos.petshop.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.marcos.petshop.domain.Categoria;
+import com.marcos.petshop.domain.Produto;
 import com.marcos.petshop.repository.CategoriaRepository;
+import com.marcos.petshop.repository.ProdutoRepository;
 
 @Component
 public class PopulaDados {
@@ -16,14 +19,62 @@ public class PopulaDados {
 	@Autowired
 	CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
 	@PostConstruct
 	public void cadastrar() {
 		
 		Categoria cat1 = new Categoria(null, "Alimento");
 		Categoria cat2 = new Categoria(null, "Remédio");
 		Categoria cat3 = new Categoria(null, "Cosmético");
+	
+		
+		Produto p1 = new Produto(null,"Ração", 100.0);
+		Produto p2 = new Produto(null,"Sache", 80.0);
+		Produto p3 = new Produto(null,"Shampoo", 150.0);
+		Produto p4 = new Produto(null,"Vermefugo", 110.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2));
+		cat2.getProdutos().addAll(Arrays.asList(p3,p4));
+		cat3.getProdutos().addAll(Arrays.asList(p4));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2));
+		p4.getCategorias().addAll(Arrays.asList(cat2,cat3));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
